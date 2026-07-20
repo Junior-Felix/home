bb4fc8a6175cac96d3c844c6a543e209f227ed95
Junior-Felix <juniorfelix540@outlook.com>
2026-07-21 01:02:42 +0300

    (feat): Adds github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Junior-Felix.github.io",
+    base: '/home',
   integrations: [
     sitemap(),
     tailwind({
